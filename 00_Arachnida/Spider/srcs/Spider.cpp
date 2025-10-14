1f53f6564c48792b9a1545d2ee30e64c50bd127c
Delphine Abouab <dabouab@f1br4s6.clusters.42paris.fr>
2025-10-14 16:59:54 +0200

    parsing method

--- 00_Arachnida/Spider/srcs/Spider.cpp
+++ 00_Arachnida/Spider/srcs/Spider.cpp
@@ -69,18 +69,81 @@ void	Spider::check(string opt) {
 	}
 }
 
+size_t write_to_string(void *ptr, size_t size, size_t nmemb, void *userdata) {
+    std::string *str = static_cast<std::string*>(userdata);
+    str->append(static_cast<char*>(ptr), size * nmemb);
+    return size * nmemb;
+}
+
+// TODO: explain in Notion
+vector<string>	parse_get_all(string content, string Xpath, string extract) {
+	htmlDocPtr 		doc;
+	vector<string>	results = {};
+
+	doc = htmlReadMemory(content.c_str(), content.length(), nullptr, nullptr, HTML_PARSE_NOERROR);
+	if (!doc)
+        throw runtime_error("Failed to parse HTML document");
+	
+  // Instantiate XPath context
+	xmlXPathContextPtr context = xmlXPathNewContext(doc);
+	if (!context) {
+		xmlFreeDoc(doc);
+        throw runtime_error("Failed to parse HTML document");
+	}
+
+	// Fetch all elements matching the given XPath
+  	xmlXPathObjectPtr elements = xmlXPathEvalExpression((xmlChar *)Xpath, context);
+	if (!elements) {
+		xmlXPathFreeContext(context);
+		xmlFreeDoc(doc);
+		throw runtime_error("Failed to parse HTML document");
+	}
+
+	if (definitionSpanElements->nodesetval == NULL)
+		return result;
+	
+	// Iterate over the elements to store them in the vector
+	for (int i = 0; i < definitionSpanElements->nodesetval->nodeNr; ++i)
+	{
+		// Get element from list
+		xmlNodePtr element = elemets->nodesetval->nodeTab[i];
+
+        xmlXPathSetContextNode(element, context);
+
+		// extract data
+		string data = xpath_extract_string(context, extract);
+
+		if (result.find(data) == result.end())
+			results.push(data);
+	}
+
+    // Free resources
+    xmlXPathFreeObject(elements);
+    xmlXPathFreeContext(context);
+    xmlFreeDoc(doc);
+
+	return results;
+}
+
 void	Spider::scrap(const string url) {
 	vector<string>	urls;
 	vector<string>	imgs;
+	string			content;
 
 	// Get the page
 	CURL *curl = curl_easy_init();
 	if(!curl)
 		throw exception();
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &content); 
     CURLcode res = curl_easy_perform(curl);
 	
+	if (res != CURLE_OK)
+		throw runtime_error(url + " is invalid or not responding");
+
 	// TODO: parse to get each image url
+
 
 	// TODO: download every images
 
