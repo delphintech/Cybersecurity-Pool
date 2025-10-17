c9643ec2e7739bf21da801a2b4acaa0c54031922
Delphine Abouab <dabouab@f1br6s10.clusters.42paris.fr>
2025-10-17 16:37:18 +0200

    done, to be tested

--- 00_Arachnida/Spider/srcs/main.py
+++ 00_Arachnida/Spider/srcs/main.py
@@ -1,38 +1,24 @@
 #!/usr/bin/env python3
 import sys
-# import spider
-# import utils
+from spider import Spider
+import utils
 
 def main():
-    print(f"COucou {sys.argv[1]}")
-    # Récupération des arguments
-    # args = sys.argv[1:]  # sys.argv[0] est le nom du script
-    # if len(args) < 2:
-    #     print(utils.USAGE)
-    #     sys.exit(1)
+	args = sys.argv[1:]
+	if not args:
+		print(utils.USAGE)
+		sys.exit(1)
 
-    # spider	spider
+	try:
+		spider = Spider(args)
+		spider.scrap(spider.url, spider.depth)
+		print(f"📥 {Spider.imgs} images downloaded")
+	except ValueError as e:
+		print(f"{e}\n\n{utils.USAGE}")
+		
+	except Exception as e:
+		print(e)
 
 # Laumch main only if called directly
 if __name__ == "__main__":
     main()
-
-
-# int	main(int ac, char **av) {
-# 	if (ac < 2) {
-# 		cerr << "No argument\n\n" << USAGE << endl;
-# 		return (EXIT_FAILURE);
-# 	}
-	
-# 	try {
-# 		Spider	spider(ac, av);
-		
-# 		spider.scrap(spider.get_url());
-# 	} catch ( invalid_argument &e) {
-# 		cerr << e.what() << "\n\n" << USAGE << endl;
-# 		return (EXIT_FAILURE);
-# 	} catch (const exception &e) {
-# 		cerr << e.what() << "\n" << endl;
-# 		return (EXIT_FAILURE);
-# 	}
-# }
--- 00_Arachnida/Spider/srcs/spider.py
+++ 00_Arachnida/Spider/srcs/spider.py
@@ -1,8 +1,11 @@
 import os
 import requests
-import beautifulsoup4
+from bs4 import BeautifulSoup
+import utils
+import urllib.parse
 
 class Spider:
+	imgs = 0
 
 	def __init__(self, args):
 		self.url = ""
@@ -53,102 +56,46 @@ class Spider:
 			raise ValueError("Invalid depth (must be between 0 and 1000)")
 		
 		''' Check for folder path '''
+		os.makedirs(self.path, exist_ok=True)
+		if self.path[-1] != '/':
+			self.path += "/"
 
+	def scrap(self, url, depth=None):
+		elements = []
+		links = []
 
-		''' Check if url is valid '''
+		if url in self.done:
+			return
+		
+		''' Get the page content '''
+		res = requests.get(url)
+		if not res.ok:
+			return
+		
+		soup = BeautifulSoup(res.text, 'html.parser')
 
-void	Spider::check(string opt) {
+		''' Parse and store each image url '''
+		elements = soup.find_all("img")
+		links = [img.get("src") for img in elements]
 
-	# Check if options validity
-	if (opt.find("l") != string::npos && opt.find("r") == string::npos)
-		throw invalid_argument("Depth option without recursive");
-	if (opt.find("l") != string::npos && (depth < 0 || depth > 1000))
-		throw invalid_argument("Invalid depth (must be between 0 and 1000)");
-	# Check for folder path validity
-	int	check = mkdir(this->path.c_str(), 0777);
-	if (this->path.back() != '/')
-		this->path += "/";
-	if (check < 0 && errno != EEXIST)
-		throw invalid_argument("Unable to create directory \"" + this->path + "\"");
-	# Check if URL is valid
-	if (url.empty())
-    	throw invalid_argument("No URL");
-	# CURL *curl = curl_easy_init();
-	# if(curl) {
-	# 	curl_easy_setopt(curl, CURLOPT_URL, this->url.c_str());
-	
-	# 	CURLcode res = curl_easy_perform(curl);
-	# 	if (res != CURLE_OK) {
-	# 		curl_easy_cleanup(curl);
-	# 		throw std::invalid_argument("URL invalid or not responding");
-	# 	}
-	# 	curl_easy_cleanup(curl);
-	# }
-}
+		''' Download each images '''
+		for x in links:
+			filename = utils.get_img_name(x)
+			if not filename or x == None:
+				continue
+			filepath = self.path + filename
+			img_url = urllib.parse.urljoin(self.url, x)
+			with open(filepath, 'wb') as file:
+				file.write(requests.get(img_url).content)
 
-
-void	Spider::scrap(const string url) {
-	vector<string>	links;
-	vector<string>	imgs;
-	string			content;
-
-	if (find(this->done.begin(), this->done.end(), url) != this->done.end())
-		return ;
-	# Get the page
-	CURL *curl = curl_easy_init();
-	if(!curl)
-		throw exception();
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
-	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &content); 
-    CURLcode res = curl_easy_perform(curl);
-	
-	if (res != CURLE_OK)
-		throw runtime_error(url + " is invalid or not responding");
-
-	curl_easy_cleanup(curl);
-
-	# Parse to get each image url
-	imgs = parse_get_all(content, "#img[@src]", "src");
-
-	# Download every images
-	for (int i = 0; i < (int)imgs.size(); i++) {
-		this->download_img(imgs[i]);
-	}
-
-	# Push url to done list
-	this->done.push_back(url);
-
-	if (this->depth > 0) {
-		# Parse to get each link in the page
-		links = parse_get_all(content, "#a[@href]", "href");
-		this->depth--;
-		# Scrap every links
-		for (int i = 0; i < (int)links.size(); i++) {
-			this->scrap(links[i]);
-		}
-	}
-}
-
-void		Spider::download_img(const string url) {
-	string	filename;
-
-	filename = get_img_name(url);
-	if (filename.empty())
-		return ;
-	
-	filename = this->path + filename;
-    CURL* curl = curl_easy_init();
-    if (curl) {
-        FILE* fp = fopen(filename.c_str(), "wb");
-        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-        CURLcode res = curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-        fclose(fp);
-
-        if (res != CURLE_OK)
-            throw runtime_error(url + "Could not download: " + url);
-	}
-}
+		self.done.append(url)
+		elements.clear()
+		links.clear()
+		''' Scrap all the links in the page'''
+		if depth > 0:
+			elements = soup.find_all("a")
+			links = [img.get("href") for img in elements]
+			for x in links:
+				if x != None and not x.startswith("#") and not x.startswith("mailto:"):
+					link_url = urllib.parse.urljoin(self.url, x)
+					self.scrap(link_url, depth - 1)
