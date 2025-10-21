3f7ec427c0d766a95166f91751f3d08313441e8a
Delphine Abouab <dabouab@f2r11s4.clusters.42paris.fr>
2025-10-21 19:21:40 +0200

    scorpion done

--- 00_Arachnida/Scorpion/srcs/main.py
+++ 00_Arachnida/Scorpion/srcs/main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 import sys
 import utils
+from termcolor import cprint
 
 def main():
 	args = sys.argv[1:]
 	if not args:
 		print(utils.USAGE)
 		sys.exit(1)
+	
+	i = 0
+	while i < len(args):
+		if utils.check_img(args[i]):
+			meta = utils.meta(args[i])
+			# utils.display_meta(meta)
+		else:
+			cprint(f"'{args[i]}' is not an image or is not readable", "red")
+		if (i < len(args) - 1):
+			print("\n\n")
+		i += 1
 
 
 # Laumch main only if called directly
--- 00_Arachnida/Scorpion/srcs/utils.py
+++ 00_Arachnida/Scorpion/srcs/utils.py
@@ -1,1 +1,41 @@
+import os
+from termcolor import cprint
+from PIL import Image
+from PIL.ExifTags import TAGS
+
 USAGE = "usage: ./scorpion FILE1 [FILE2 ...]\n"
+
+def check_img(file: str) -> bool:
+	exts = (".jpg", ".jpeg", ".png", ".gif", ".bmp")
+
+	for x in exts:
+		if file.endswith(x):
+			if os.access(file, os.R_OK):
+				return True
+	return False
+
+def	meta(file: str):
+	meta = {}
+	image = Image.open(file)
+	cprint(f"---------- {image.filename} ----------", "blue", "on_white")
+	print(f"Size: {image.size}")
+	print(f"Dimension: {image.height} x {image.width}")
+	print(f"Format: {image.format}\n")
+	exifdata = image.getexif()
+	if not exifdata:
+		cprint("This image has no EXIF", "black", "on_white")
+		image.close()
+		return
+	cprint("EXIF:", "black", "on_white")
+	for tag_id in exifdata:
+		tag = TAGS.get(tag_id, tag_id)
+		data = exifdata.get(tag_id)
+		if isinstance(data, bytes):
+			data = data.decode()
+		print(f"{tag}: {data}")
+	image.close()
+	
+
+
+# def display_meta(meta: dict):
+# 	print("")
