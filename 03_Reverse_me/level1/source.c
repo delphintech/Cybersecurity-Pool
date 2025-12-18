8bdfe4fb4d539d665d7377baf35829f9cf8e007c
Dabouab <dabouab@student.42.fr>
2025-12-18 18:09:59 +0100

    level1

--- 03_Reverse_me/level1/source.c
+++ 03_Reverse_me/level1/source.c
@@ -6,10 +6,22 @@
 /*   By: dabouab <dabouab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 15:41:38 by dabouab           #+#    #+#             */
-/*   Updated: 2025/12/18 15:42:02 by dabouab          ###   ########.fr       */
+/*   Updated: 2025/12/18 18:09:31 by dabouab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+
 int	main(void) {
+	char *pwd = "__stack_check";
+	char buf[24];
 	
+	printf("Please enter key: ");
+	scanf("%s", &buf[0]);
+	if (strcmp(pwd, buf) == 0) {
+		printf("Good job.\n");
+	} else {
+		printf("Nope.\n");
+	}
 }
