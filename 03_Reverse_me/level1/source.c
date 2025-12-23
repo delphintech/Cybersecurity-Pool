babebc63efec3194c971e0143fb6e7cf873389b8
Dabouab <dabouab@student.42.fr>
2025-12-23 18:23:51 +0100

    level2 struggling

--- 03_Reverse_me/level1/source.c
+++ 03_Reverse_me/level1/source.c
@@ -6,7 +6,7 @@
 /*   By: dabouab <dabouab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 15:41:38 by dabouab           #+#    #+#             */
-/*   Updated: 2025/12/18 18:09:31 by dabouab          ###   ########.fr       */
+/*   Updated: 2025/12/23 17:49:34 by dabouab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 int	main(void) {
 	char *pwd = "__stack_check";
-	char buf[24];
+	char input[32];
 	
 	printf("Please enter key: ");
-	scanf("%s", &buf[0]);
-	if (strcmp(pwd, buf) == 0) {
+	scanf("%s", &input[0]);
+	if (strcmp(pwd, input) == 0) {
 		printf("Good job.\n");
 	} else {
 		printf("Nope.\n");
--- 03_Reverse_me/level2/source.c
+++ 03_Reverse_me/level2/source.c
@@ -6,10 +6,69 @@
 /*   By: dabouab <dabouab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 15:55:35 by dabouab           #+#    #+#             */
-/*   Updated: 2025/12/18 15:55:37 by dabouab          ###   ########.fr       */
+/*   Updated: 2025/12/23 18:05:23 by dabouab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void) {
-	
+#include <stdio.h>
+#include <stdlib.h>
+
+void	no() {
+	printf("Nope.");
+	exit(1);
 }
+
+void	ok() {
+	printf("Good job.");
+	exit(0);
+}
+
+int	main(void) {
+	char	input[32];
+	char	buf[9]
+	int 	ret;
+
+	printf("Please enter key: ");
+	if (scanf("%s", &input[0]) == 1) {
+		if (input[1] == '0')
+			if (input[0] == '0')
+				memset(buf, 0, 9);
+				buf[0] = "d"
+				if (strlen(&buf[0]) >= 8)
+				else
+					
+			else 
+				no();
+		else
+			no();
+	} else 
+		no();
+}
+
+
+// strlen(&input[0]);
+
+// atoi($input[0]);
+
+// strmcp("delabere", );
+
+// 0012345678delabere
+
+// ebp-0x40 ""
+// 0xc
+
+// ecx = "0"
+// ebp-0x35 = argument
+// ebx-0xc = "@ ."
+
+
+
+
+// eax = 64  ("@")
+// eax = 32  (" ")
+// eax = -120
+
+// "%23s"
+
+
+// strlen("d")
