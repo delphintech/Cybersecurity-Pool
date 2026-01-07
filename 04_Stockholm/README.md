263f762522b13def57129785229dbc3ab9faeeb9
Dabouab <dabouab@student.42.fr>
2026-01-07 17:11:00 +0100

    init Stockholm

---
+++ 04_Stockholm/README.md
@@ -0,0 +1,11 @@
+# SCORPION
+The scorpion program receive image files as parameters and display the EXIF and metadata.
+
+## Create and handle program
+
+`make` Create environment and executable
+
+`make clean` stop environment and delete exec
+
+## Program usage
+    ./scorpion FILE1 [FILE2 ...]
---
+++ 04_Stockholm/srcs/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+def main():
+	print("coucou")
+
+# 	args = sys.argv[1:]
+# 	if not args:
+# 		print(utils.USAGE)
+# 		sys.exit(1)
+	
+# 	i = 0
+# 	while i < len(args):
+# 		if utils.check_img(args[i]):
+# 			meta = utils.meta(args[i])
+# 		else:
+# 			cprint(f"'{args[i]}' is not an image or is not readable", "red")
+# 		if (i < len(args) - 1):
+# 			print("\n\n")
+# 		i += 1
+
+
+# Laumch main only if called directly
+if __name__ == "__main__":
+    main()
